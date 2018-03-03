b08599abcc73b1a94d2ff484b2e74377c835eeac
logonrm <logonrm@fiap.com.br>
2018-03-03 16:42:12 -0300

    Initial commit

---
+++ app/src/main/java/com/example/logonrm/demoaacretrofit/mainscreen/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.example.logonrm.demoaacretrofit.mainscreen
+
+import android.arch.lifecycle.Observer
+import android.arch.lifecycle.ViewModelProviders
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import com.example.logonrm.demoaacretrofit.R
+import com.example.logonrm.demoaacretrofit.ui.mainscreen.MainViewModel
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var mainViewModel: MainViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        mainViewModel = ViewModelProviders.of(
+                this).get(MainViewModel::class.java)
+
+        btPesquisar.setOnClickListener {
+            mainViewModel.pesquisarEndereco(etCEP.text.toString())
+        }
+
+        mainViewModel.apiResponse.observe(this, Observer{
+
+            apiResponse ->
+            if (apiResponse?.erro == null){
+                val end = apiResponse?.endereco
+                tvResultado.text =
+                        "Logradouro: ${end?.logradouro}\n" +
+                         "Complemento: ${end?.complemento}" +
+                         "Bairro: ${end?.bairro}" +
+                         "Localidade: ${end?.localidade}" +
+                         "UF: ${end?.uf}"
+            }
+            else{
+                Log.i("TAG", "${apiResponse.erro}")
+            }
+        }
+        )
+    }
+}
